78325e8d7b73986b55d732b2d521fd6701c87409
Troy Nechanicky <nech5860@mylaurier.ca>
2018-04-03 10:25:19 -0400

    Moved js to extensionControllers.js. Modified CSS so that it works nice as extension popup. Finished basic login js.

--- extension/js/extensionControllers.js
+++ extension/js/extensionControllers.js
@@ -1,1 +1,130 @@
+var serverUrl = "https://laurierlink.herokuapp.com/";
+
+//Initialize collapsible elements
+var elem = $('.collapsible');
+var instance = M.Collapsible.init(elem, null);
+
+//Initialize select elements
+var elem = $('select');
+var instance = M.FormSelect.init(elem, null);
+
+
 //listeners for extension.html
+
+//Toggle accordian icons. Look for .svg since fa js inserts svg into i
+$('#action-accordion .collapsible-header').click(function () {
+	$(this).find('svg').toggleClass('fa-caret-right fa-caret-down');
+})
+
+//Handle click on forgot-password a
+$('#forgot-password').click(function (event) {
+	event.preventDefault();
+})
+
+//disable form submittion
+$('form').submit(false);
+
+//on login submit
+//rely on browser to do front-end validation (back-end will do final validation)
+//trigger fade-in fade-out success modal on good login
+$('#login-submit').click(function (event) {
+	//check to make sure that the browser hasn't marked any of the fields invalid
+	//won't be marked invalid if user hasn't clicked in it, so check length too
+	if ($('#login-form input').hasClass('invalid') ||
+		$('#login-email').val().length === 0 || $('#login-password').val().length === 0) {
+		return;
+	}
+
+	/*             var post = $.post(serverUrl + "login-controller", function () {});
+				post.fail(function () {
+					console.log("Login request to server failed");
+				});
+				post.done(function (data) {
+					if (data === "valid") { */
+	var modal = $('#login-success-modal');
+	var instance = M.Modal.init(modal, {
+		'inDuration': 1000,
+		'outDuration': 1000,
+		'onCloseEnd': loggedIn
+	});
+	instance = M.Modal.getInstance(modal);
+	instance.open()
+	modal.blur();
+
+	setTimeout(function () {
+		instance.close();
+	}, 1000);
+	/*  } else if (data === "invalid") {
+		alert("Your login is invalid!");
+	}
+	else {
+		console.log("Unexepected data from server");
+	}
+	}
+	 }) 
+	 */
+});
+
+//register button listener
+$('#registration-submit').click(function (event) {
+	event.preventDefault();
+
+	//show confirm password field if not already
+	if ($('#confirm-password-wrapper').hasClass('hide')) {
+		$('#confirm-password-wrapper').addClass('hide');
+		return;
+	}
+
+	//else, check to see if any fields invalid
+	if ($('#login-form input').hasClass('invalid') || $('#login-email').val().length === 0) {
+		return;
+	}
+
+	//else, check if passwords match
+	//if not, make sure confirm-password is marked invalid
+	if ($('#confirm-password').val().length === 0 ||
+		$('#login-password').val() !== $('#confirm-password').val()) {
+		$('#confirm-password').addClass('invalid');
+		return;
+	}
+
+	//else, passwords match
+	//send to server
+	var post = $.post(serverUrl + "registration-controller", function () {});
+	post.fail(function () {
+		console.log("Registration request to server failed");
+	});
+	post.done(function (data) {
+		if (data === "valid") {
+			var modal = $('#registration-msg');
+			var instance = M.Modal.init(modal, {
+				'inDuration': 1000,
+				'outDuration': 1000,
+			});
+			instance = M.Modal.getInstance(modal);
+			instance.open()
+			modal.blur();
+
+			setTimeout(function () {
+				instance.close();
+			}, 2000);
+		} else if (data === "invalid") {
+			alert("Your login is invalid!");
+		} else {
+			console.log("Unexepected data from server");
+		}
+	});
+});
+
+function loggedIn() {
+	//write to memory that logged in
+	
+	//switch login tab to logout
+	$('#login-tab').fadeOut(400, function() {
+		$('#login-tab').addClass('hide');
+		$('#logout-tab').removeClass('hide');
+		$('#logout-tab').hide();
+		$('#logout-tab').fadeIn();
+	});
+	
+}
