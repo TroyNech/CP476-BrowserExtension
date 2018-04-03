6d035dfe2d3738b7c63cdd53073d5ab4cd4680e3
joshua-nguyen <nguy1920@mylaurier.ca>
2018-04-03 15:25:19 -0400

    Add files via upload
    First commit, javascript. need to find out how to auto-nav through webpages

---
+++ background.js
@@ -0,0 +1,6 @@
+function run(){
+    var myWindow = window.open("", "MsgWindow", "width=200,height=100");
+    myWindow.alert("fdsaf");
+}
+
+run();
---
+++ courseInfo.js
@@ -0,0 +1,38 @@
+function courseInfo(){
+    /*
+        The following will be hard coded as placeholders until actual implementation and results are available
+        https://loris.wlu.ca/ssb_prod/bwckctlg.p_disp_course_detail?cat_term_in=201805&subj_code_in=CP&crse_numb_in=104
+    */
+    //login
+    //window.open("https://loris.wlu.ca","_blank");
+    
+var link1 = "https://loris.wlu.ca/ssb_prod/bwckctlg.p_disp_course_detail?cat_term_in=";
+    var link2 = "&subj_code_in=";
+    var link3 = "&crse_numb_in=";
+    
+    var term = 201805;
+    var subj = "CP";
+    var course = "104";
+    var main = window.open(link1 + term + link2 + subj + link3 + course,"_blank");
+    
+    
+    //======================================================================
+    var title;
+    var content;
+    
+    title = document.getElementsByClassName('nttitle');
+    content = document.getElementsByClassName('ntdefault');
+    
+    //======================================================================
+    
+    var courseWindow = window.open("", "courseWindow", "width=1080,height=720");
+    courseWindow.document.write('<p>'+title[0].innerHTML+'</p>');
+    courseWindow.document.write('<p>'+content[0].innerHTML+'</p>');
+}
+/*
+var link = window.location.href;
+var curr = "https://loris.wlu.ca/ssb_prod/bwckctlg.p_disp_course_detail?cat_term_in=";
+if (link.includes(curr)){
+    courseInfo();
+}*/
+    courseInfo();
